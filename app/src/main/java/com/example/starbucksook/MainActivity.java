585052f5423faf64681a4e0ea1050a380b52bf50
Serin-Kim <serin9864@gmail.com>
2021-11-26 17:40:33 +0900

    MenuActivity 구현

--- app/src/main/java/com/example/starbucksook/MainActivity.java
+++ app/src/main/java/com/example/starbucksook/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.starbucksook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btn = (Button)findViewById(R.id.start_activity_main_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                String idtext;
+//                String pwtext;
+//
+//                idtext=edit_id.getText().toString();
+//                pwtext=edit_pw.getText().toString();
+
+                //objTV.setText(idtext+"님 \n Book N Study에 오신 것을 환영합니다!");
+
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+
+//                Bundle extras = new Bundle();
+//                extras.putString("id", idtext);
+//                extras.putString("pw", pwtext);
+
+//                intent.putExtras(extras);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
 
--- app/src/main/java/com/example/starbucksook/MenuActivity.java
+++ app/src/main/java/com/example/starbucksook/MenuActivity.java
@@ -3,12 +3,33 @@ package com.example.starbucksook;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MenuActivity extends AppCompatActivity {
 
+        RecyclerView recyclerView;
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-    }
+        String s1[], s2[];
+//        int images[] = {R.drawable.coffee1, R.drawable.coffee2, R.drawable.coffee3};
+        int images[] = {R.drawable.coffee1, R.drawable.coffee2, R.drawable.coffee3, R.drawable.black_cat, R.drawable.cow, R.drawable.deer, R.drawable.dog, R.drawable.eagle, R.drawable.elephant,  R.drawable.fish_in_glass, R.drawable.hunter, R.drawable.lion};
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_menu);
+
+            recyclerView = findViewById(R.id.recyclerView);
+
+//            s1 = getResources().getStringArray(R.array.menu_name);
+//            s2 = getResources().getStringArray(R.array.menu_description);
+            s1 = getResources().getStringArray(R.array.animal_name);
+            s2 = getResources().getStringArray(R.array.description);
+
+            MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+            recyclerView.setAdapter(myAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        }
+
+
+
 }
